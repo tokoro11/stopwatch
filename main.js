58c04f97288dd9cc3b564155b7988d8ac86b9f22
所宏佑紀 <tokorohiroshiyuuki@tokorohiroshiyuukinoMacBook-Air.local>
2025-12-01 14:09:10 +0900

    js変更

--- main.js
+++ main.js
@@ -36,49 +36,25 @@ function stopWatch(){
    interval = setInterval(stopWatch, 100);
   }
   )
- 
+  
   
   
  stop.addEventListener("click", function(){
   clearInterval(interval);
-}
-)
+
+  }
+  )
 
 reset.addEventListener("click", function(){
   clearInterval(interval);
   display.innerHTML = "0:0:0:0";
-}
-)
+  $("#start").prop('disabled', false );
+  }
+  )
 
-// $(document).ready(function(){
-//   $("#start").click(function() {
-//     $("#start").prop('disabled', true);
+$(document).ready(function(){
+  $("#start").click(function() {
+  $("#start").prop('disabled', true);
 
-// })
-// })
-
-
-// function clickBtn1(){
-	
-// if (document.getElementById("clickBtn2").disabled === true){
-//   document.getElementById("clickBtn2").removeAttribute("disabled");;
-
-// }else{
-//   // disabled属性を設定
-//   document.getElementById("clickBtn2").setAttribute("disabled", true);
- 
-// }
-// }
-
-// #stop.prop( 'disabled', true )
-
-
-
-
-// $(function() {
-//   $("#start").change(function() {
-//     $(button).prop('disabled', true);
-//   });
-// });
-
-// $("#pugi").prop("disabled", false);
+  })
+  })
