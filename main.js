6bb7aa2c021e09444e8a60f61601fa5839bca247
所宏佑紀 <tokorohiroshiyuuki@tokorohiroshiyuukinoMacBook-Air.local>
2025-12-02 20:19:42 +0900

    syuusei

--- main.js
+++ main.js
@@ -1,60 +1,56 @@
-let display = document.getElementById("display");
-let start = document.getElementById("start");
-let stop = document.getElementById("stop");
-let reset = document.getElementById("reset");
+$(function() {
+  var fps =0;
+  var sec =0;
+  var min =0;
+  var hour =0;
+  var timer;
+ 
+$(".start").click(function() {
+  timer = setInterval(countup, 100);
 
-let hours = 0;
-let minutes = 0;
-let seconds =0;
-let aSecond =0;
+  $(this).prop("disabled", true);
+  $(".stop,.reset").prop("disabled", false);
+});
 
-// let status;
-// let interval;
+$(".stop").click(function(){
+  clearInterval(timer);
 
-function stopWatch(){
- aSecond++;
- if(aSecond /10 == 1){
-   seconds++;
-   aSecond = 0;
-  
- if(seconds / 60 == 1){
-    minutes++;
-    seconds = 0;
+  $(this).prop("disabled", true);
+  $(".start").prop("disabled", false);
+})
 
-     
- if(minutes / 60 == 1){
-    hours++;
-    minutes = 0;
+$(".reset").click(function() {
+  fps = 0;
+  sec = 0;
+  min = 0;
+  hour =0;
+
+  $(".clock").html("0:0:0:0");
+  clearInterval(timer);
+
+  $(".stop,.reset").prop("disabled", true);
+  $(".start").prop("disabled", false);
+});
+
+ function countup()
+  {
+    fps += 1;
+
+    if (fps > 9) {
+      fps = 0;
+      sec += 1;
+    }
+
+    if (sec > 59) {
+      sec = 0;
+      min += 1;
+    }
+    
+    if (min > 59) {
+      min = 0;
+      hour += 1;
+    }
+    
+  $(".clock").html(hour + ':' + min + ':' +  sec + ':' + fps);
   }
-  }
-  }
-
-  display.innerHTML = hours + ":" + minutes + ":" + seconds + ":" + aSecond; 
-  }
-
- start.addEventListener("click", function(){
-   interval = setInterval(stopWatch, 100);
-  }
-  )
-  
-  
-  
- stop.addEventListener("click", function(){
-  clearInterval(interval);
-
-  }
-  )
-
-reset.addEventListener("click", function(){
-  clearInterval(interval);
-  display.innerHTML = "0:0:0:0";
-  $("#start").prop('disabled', false );
-  }
-  )
-
-$(document).ready(function(){
-  $("#start").click(function() {
-  $("#start").prop('disabled', true);
-
-  })
-  })
+});
